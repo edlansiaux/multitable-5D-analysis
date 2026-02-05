1cc9765f5546d6f7a07c3be547591d4e946efb6f
Edouard Lansiaux <edouard.lansiaux@orange.fr>
2026-02-05 16:32:49 +0100

    Update __init__.py

--- mt5d/datasets/__init__.py
+++ mt5d/datasets/__init__.py
@@ -1,3 +1,21 @@
 """
-Dataset loaders and utilities.
+Modules pour charger et gérer les datasets multitable
 """
+
+from .base import MT5DDataset, DatasetRegistry
+from .mimic import MIMICLoader
+from .amazon import AmazonReviewsLoader
+from .financial import FinancialTransactionsLoader
+from .synthetic import SyntheticDataGenerator
+from .utils import download_dataset, preprocess_dataset
+
+__all__ = [
+    'MT5DDataset',
+    'DatasetRegistry',
+    'MIMICLoader',
+    'AmazonReviewsLoader',
+    'FinancialTransactionsLoader',
+    'SyntheticDataGenerator',
+    'download_dataset',
+    'preprocess_dataset'
+]
