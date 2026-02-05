20232a82cd578c43722417475025bf18802db388
Edouard Lansiaux <edouard.lansiaux@orange.fr>
2026-02-05 15:56:33 +0100

    Create __init__.py

---
+++ mt5d/__init__.py
@@ -0,0 +1,10 @@
+"""
+MT5D Framework: Multi-Table 5D Analysis Framework
+"""
+
+from mt5d.core import DimensionalProfiler, RelationalHypergraphBuilder
+from mt5d.models import PentE, RelationalHypergraphTransformer
+from mt5d.core.pipeline import MT5DPipeline
+
+__version__ = "0.1.0-alpha"
+__author__ = "MT5D Research Team"
