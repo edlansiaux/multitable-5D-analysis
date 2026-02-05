5f724a2f825d24bb425d43fcb038a26d2ee37ae0
Edouard Lansiaux <edouard.lansiaux@orange.fr>
2026-02-05 15:58:41 +0100

    Create __init__.py

---
+++ mt5d/models/__init__.py
@@ -0,0 +1,6 @@
+"""
+Model architectures and embeddings.
+"""
+
+from mt5d.models.embeddings import PentE, HighCardinalityEncoder, TemporalEncoder
+from mt5d.models.architectures import RelationalHypergraphTransformer
