93f9aaea1b15d22e97201a75d41c2c66e67e12c3
Edouard Lansiaux <edouard.lansiaux@orange.fr>
2026-02-05 15:57:00 +0100

    Create __init__.py

---
+++ mt5d/core/__init__.py
@@ -0,0 +1,7 @@
+"""
+Core modules for the MT5D framework.
+"""
+
+from mt5d.core.profiling import DimensionalProfiler
+from mt5d.core.hypergraph import RelationalHypergraphBuilder
+from mt5d.core.pipeline import MT5DPipeline
