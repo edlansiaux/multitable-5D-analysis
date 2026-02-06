11f093fdba3d54bf572bf2ae48fd54c87953aed4
Edouard Lansiaux <edouard.lansiaux@orange.fr>
2026-02-06 09:21:30 +0100

    Update __init__.py

--- mt5d/models/embeddings/__init__.py
+++ mt5d/models/embeddings/__init__.py
@@ -2,4 +2,4 @@
 Embedding modules for the 5 dimensions.
 """
 
-from mt5d.models.embeddings.pente import PentE, HighCardinalityEncoder, TemporalEncoder
+from mt5d.models.embeddings.pente import PentE
