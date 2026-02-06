2d4a4387b8b27e6455997dde8b1f0ec6ed043958
Edouard Lansiaux <edouard.lansiaux@orange.fr>
2026-02-06 09:28:22 +0100

    Update __init__.py

--- mt5d/core/__init__.py
+++ mt5d/core/__init__.py
@@ -1,4 +1,18 @@
-"""
-Core modules for the MT5D framework.
-"""
+from . import pipeline
+from . import profiling
+from . import hypergraph
+from . import causal
+from . import explainability
+from . import federated
+from . import ops
 
+# Liste des modules publics lors d'un 'from mt5d.core import *'
+__all__ = [
+    "pipeline",
+    "profiling",
+    "hypergraph",
+    "causal",
+    "explainability",
+    "federated",
+    "ops"
+]
