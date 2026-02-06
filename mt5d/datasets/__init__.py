bdff03813dfaeb9256894cb0b170e4188ef9411a
Edouard Lansiaux <edouard.lansiaux@orange.fr>
2026-02-06 09:22:57 +0100

    Update __init__.py

--- mt5d/datasets/__init__.py
+++ mt5d/datasets/__init__.py
@@ -1,21 +1,5 @@
-"""
-Modules pour charger et gérer les datasets multitable
-"""
-
-from .base import MT5DDataset, DatasetRegistry
-from .mimic import MIMICLoader
-from .amazon import AmazonReviewsLoader
-from .financial import FinancialTransactionsLoader
-from .synthetic import SyntheticDataGenerator
-from .utils import download_dataset, preprocess_dataset
-
-__all__ = [
-    'MT5DDataset',
-    'DatasetRegistry',
-    'MIMICLoader',
-    'AmazonReviewsLoader',
-    'FinancialTransactionsLoader',
-    'SyntheticDataGenerator',
-    'download_dataset',
-    'preprocess_dataset'
-]
+from .base import BaseDataset
+from .synthetic import SyntheticMultiTableGenerator
+from .mimic import MIMICDataset
+from .amazon import AmazonDataset
+from .financial import FinancialDataset
