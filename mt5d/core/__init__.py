8aa913edfced9db2f8a234e8058a918930d98646
Edouard Lansiaux <edouard.lansiaux@orange.fr>
2026-02-06 09:19:03 +0100

    Update __init__.py

--- mt5d/core/__init__.py
+++ mt5d/core/__init__.py
@@ -2,6 +2,3 @@
 Core modules for the MT5D framework.
 """
 
-from mt5d.core.profiling import DimensionalProfiler
-from mt5d.core.hypergraph import RelationalHypergraphBuilder
-from mt5d.core.pipeline import MT5DPipeline
